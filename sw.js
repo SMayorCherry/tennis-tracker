03d0b22d13f2515ce28652832eed3ffb30028308
SMayorCherry <shashank.mayor@gmail.com>
2025-07-30 08:19:48 +0530

    Add tennis tracker files
    Add tennis tracker files

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'tennis-tracker-v1';
+const urlsToCache = [
+  './tennis-tracker.html',
+  './manifest.json'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+}); 
